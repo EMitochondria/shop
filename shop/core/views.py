d6403fde11f311b1dc6bcf4c7058d2840473ec41
Syldys Sunduyool <Syldys.Sunduyool@icl-services.com>
2023-01-27 09:36:59 +0300

    fix templates

--- shop/core/views.py
+++ shop/core/views.py
@@ -5,3 +5,8 @@ from django.template import loader
 def index(request):
     template = loader.get_template('core/index.html')
     return HttpResponse(template.render({}, request))
+
+
+def about(request):
+    template = loader.get_template('core/about.html')
+    return HttpResponse(template.render({}, request))
